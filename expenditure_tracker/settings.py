baca75ced065cfe72cfeb11c9c1280c5769262aa
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2025-02-19 19:51:11 +0300

    collectstatic fix

--- expenditure_tracker/settings.py
+++ expenditure_tracker/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collectstatic
 STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, 'static'),  # Actual static files directory
