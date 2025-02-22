fada7951e44750582b6d26783d6fed420b4dcb0c
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2025-02-22 20:56:51 +0300

    Fix

--- expenditure_tracker/wsgi.py
+++ expenditure_tracker/wsgi.py
@@ -15,6 +15,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expenditure_tracker.settings')
 
 application = get_wsgi_application()
 
+app = application
+
 # When using pythonanywhere
 # import os
 # import sys
