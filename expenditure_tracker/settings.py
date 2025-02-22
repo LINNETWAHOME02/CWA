22a665406440d92b3aa60360a897ac7fa4d61850
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2025-02-22 10:07:32 +0300

    Fix

--- expenditure_tracker/settings.py
+++ expenditure_tracker/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+# import os
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collectstatic
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collectstatic
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Simplified WhiteNoise configuration for Render
