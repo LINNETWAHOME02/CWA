4b902bb84374c6739000e6c5e041f0dfb5a3f4e4
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2025-02-22 20:54:30 +0300

    Fix

--- expenditure_tracker/wsgi.py
+++ expenditure_tracker/wsgi.py
@@ -7,22 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expenditure_tracker.settings')
-#
-# application = get_wsgi_application()
-
 import os
-import sys
-
-path = '/home/cwa/CWA'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'expenditure_tracker.settings'
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expenditure_tracker.settings')
+
 application = get_wsgi_application()
+
+# When using pythonanywhere
+# import os
+# import sys
+#
+# path = '/home/cwa/CWA'
+# if path not in sys.path:
+#     sys.path.append(path)
+#
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'expenditure_tracker.settings'
+#
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
